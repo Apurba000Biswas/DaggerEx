5793292cf6a808ab70bd1c07c23ebb7ca7c5f824
apurba <apurba10024@gmail.com>
2020-11-28 23:42:37 +0600

    Added Dagger base bone

---
+++ app/src/main/java/com/apurba/daggerex/di/AppComponent.java
@@ -0,0 +1,30 @@
+package com.apurba.daggerex.di;
+
+
+import android.app.Application;
+
+import com.apurba.daggerex.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        // BindsInstance is not mandatory
+        @BindsInstance
+        Builder application(Application application);
+
+        // calling build() is mandatory
+        AppComponent build();
+    }
+}
